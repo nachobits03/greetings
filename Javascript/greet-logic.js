75e9be5cfd87d961839283eaa94ddad755f53547
coder <nathrijacobs@gmail.com>
2018-06-12 11:23:35 +0200

    completed tests

--- Javascript/greet-logic.js
+++ Javascript/greet-logic.js
@@ -5,23 +5,22 @@ var GreetUser = function(storedNames) {
   var greet; // <----
 
 
-
   var greetMe = function(name, language) {
     var one = name.trim().toUpperCase();
 
 if (one === ""){
-  greet = "Insert a Name"
+  greet = "Insert a Name";
 }
 else if (isNaN(name) === false){
-  greet = "Your name cannot contain any numbers"
+  greet = "Your name cannot contain any numbers";
 }
 else if (one !== "" && language === undefined){
-  greet = "Select a Language"
+  greet = "Select a Language";
 }
 
     if (isNaN(one) === true && language !== undefined) {
       if (storedNamesList[one] === undefined) {
-        storedNamesList[one] = 0
+        storedNamesList[one] = 0;
       }
 
       if (language == "English") {
@@ -39,18 +38,17 @@ else if (one !== "" && language === undefined){
   };
 
 
-
   var respond = function() {
     return greet;
   };
 
   var namesList = function() {
     return storedNamesList;
-  }
+  };
 
   var counter = function() {
-    return Object.keys(storedNamesList).length
-  }
+    return Object.keys(storedNamesList).length;
+  };
   return {
     greetMe: greetMe,
     counter: counter,
--- TDD/greeting-test.js
+++ TDD/greeting-test.js
@@ -1,18 +1,18 @@
 describe("Testing greeting logic", function() {
 
   it("should return nathri", function() {
-    var greet = GreetUser()
-    greet.greetMe("Nathri", "English")
-    assert.equal(greet.respond(), "Hello, Nathri")
-  })
+    var greet = GreetUser();
+    greet.greetMe("Nathri", "English");
+    assert.equal(greet.respond(), "Hello, Nathri");
+  });
 
 
   it("should greet using selected langauge and name and return 1 name greeted and ignoring blank spaces", function() {
     var greet = GreetUser();
 
-    greet.greetMe("Nathri", "Italian")
-    greet.greetMe("Nathri", "English")
-    greet.greetMe("Nathri", "Japanese")
+    greet.greetMe("Nathri", "Italian");
+    greet.greetMe("Nathri", "English");
+    greet.greetMe("Nathri", "Japanese");
 
     assert.equal(greet.respond(), "Kon'nichiwa, Nathri");
     assert.equal(greet.counter(), 1);
@@ -22,21 +22,20 @@ describe("Testing greeting logic", function() {
     var greet = GreetUser();
     greet.greetMe("Nathri", "Italian");
     greet.greetMe("Thomas", "Japanese");
-    assert.equal(greet.respond(), "Kon'nichiwa, Thomas")
+    assert.equal(greet.respond(), "Kon'nichiwa, Thomas");
     assert.equal(greet.counter(), 2);
   });
 
-  it("should greet using selected langauge and name and return 3 names greeted", function() {
+  it("should return 3 names already in storage", function() {
     var greet = GreetUser(["NATHRI", "THOMAS", "SAMUEL"]);
-    greet.greetMe("NATHRI", "Italian");
     assert.equal(greet.counter(), 3);
   });
 
-  it("should return 4 names greeted", function() {
+  it("should return 5 names greeted", function() {
     var greet = GreetUser(["NATHRI", "SANDY", "AKUMA", "ROBERT"]);
-    assert.equal(4, greet.counter());
+    greet.greetMe("David", "Italian");
+    assert.equal(5, greet.counter());
   });
-
 
 
 
