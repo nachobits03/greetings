11cca9118a0742b6ddb8bff639350c3d3fbe72fe
coder <nathrijacobs@gmail.com>
2018-06-14 08:51:49 +0200

    added more tests

--- TDD/greeting-test.js
+++ TDD/greeting-test.js
@@ -7,13 +7,19 @@ describe("Testing greeting logic", function() {
   });
 
 
+  it("should return 1 name counted", function() {
+    var greet = GreetUser(["NATHRI"]);
+
+    assert.equal(greet.counter(), 1);
+  });
+
+
   it("should greet 1 name in three languaages and return 1 name", function() {
     var greet = GreetUser();
 
     greet.greetMe("Nathri", "Italian");
     greet.greetMe("Nathri", "English");
     greet.greetMe("Nathri", "Japanese");
-
     assert.equal(greet.respond(), "Kon'nichiwa, Nathri");
     assert.equal(greet.counter(), 1);
   });
@@ -24,6 +30,16 @@ describe("Testing greeting logic", function() {
     greet.greetMe("Thomas", "Japanese");
     assert.equal(greet.respond(), "Kon'nichiwa, Thomas");
     assert.equal(greet.counter(), 2);
+  });
+
+  it("should return an object of the names greeted", function() {
+    var greet = GreetUser();
+    greet.greetMe("Nathri", "Italian");
+    greet.greetMe("Thomas", "Japanese");
+    greet.greetMe("Taslin", "English");
+
+    assert.deepEqual(greet.namesList(), {'NATHRI':0, 'THOMAS':0,'TASLIN':0});
+    assert.equal(greet.counter(), 3);
   });
 
   it("should return 3 names already in storage", function() {
@@ -37,6 +53,15 @@ describe("Testing greeting logic", function() {
     assert.equal(5, greet.counter());
   });
 
+  it("should return 8 names greeted and greet the last name", function() {
+    var greet = GreetUser(["NATHRI", "SANDY", "AKUMA", "ROBERT"]);
+    greet.greetMe("David", "Italian");
+    greet.greetMe("Thomas", "Japanese");
+    greet.greetMe("Sarah", "English");
+    greet.greetMe("Leilah", "Japanese");
+    assert.equal(8, greet.counter());
+    assert.equal(greet.respond(), "Kon'nichiwa, Leilah");
+  });
 
 
 });
